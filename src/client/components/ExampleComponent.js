a075c861060882234b4e3abcbe591360c7de5527
administrator <a.bozhchenko@gmail.com>
2022-11-08 16:06:41 +0200

    upd: Modify example

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to My Project</h1>;
+	return <h1>Welcome to the Project modified by Oleksii Bozhchenko</h1>;
 }
